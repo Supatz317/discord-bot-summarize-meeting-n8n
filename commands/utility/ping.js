11878681f55708dde87fd13f0c9da8e6fbb4fb16
Supatz317 <supatsorn.su@kkumail.com>
2025-05-22 10:12:48 +0700

    change Ephemeral up to date format

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply({content:'Pong! แช่', ephemeral: true});
+		await interaction.reply({content:'Pong! แช่', flags: MessageFlags.Ephemeral });
 	},
 };
