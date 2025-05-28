37bff3eaaa9161e369ac8f66f1996629564fa5f4
Supatz317 <supatsorn.su@kkumail.com>
2025-05-28 09:52:33 +0700

    cache when start server

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,16 @@
 const { Events } = require('discord.js');
+const { setCache, getChannel, getCache } = require('../database/db');
+
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		const channels = getChannel(); // or await queryChannel() if that's your function
+        setCache('channels', channels);
+        console.log('Channel list cached on startup.');
+
 	},
 };
